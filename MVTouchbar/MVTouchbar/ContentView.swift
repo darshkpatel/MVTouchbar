075ffad742e2ccc747a3b705ba04ee2227c1ce2f
Darsh <darshkpatel@gmail.com>
2020-11-24 13:49:47 +0530

    SwiftUI Boilerplate

---
+++ MVTouchbar/MVTouchbar/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MVTouchBar
+//
+//  Created by darsh on 24/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
