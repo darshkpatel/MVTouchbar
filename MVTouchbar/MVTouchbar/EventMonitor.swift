4833c65b7a4755e7ea64a6bd1c71edf4d59b71b3
Darsh <darshkpatel@gmail.com>
2020-11-24 18:05:41 +0530

    Fix event monitor

--- MVTouchbar/MVTouchbar/EventMonitor.swift
+++ MVTouchbar/MVTouchbar/EventMonitor.swift
@@ -9,26 +9,26 @@ import Cocoa
 
 class EventMonitor {
     private var monitor: Any?
-       private let mask: NSEvent.EventTypeMask
-       private let handler: (NSEvent?) -> Void
-       
-       public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
-         self.mask = mask
-         self.handler = handler
-       }
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
+    
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+      self.mask = mask
+      self.handler = handler
+    }
 
-       deinit {
-         stop()
-       }
+    deinit {
+      stop()
+    }
 
-       public func start() {
-           monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler) as! NSObject
-       }
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler) as! NSObject
+    }
 
-       public func stop() {
-         if monitor != nil {
-           NSEvent.removeMonitor(monitor!)
-           monitor = nil
-         }
-       }
+    public func stop() {
+      if monitor != nil {
+        NSEvent.removeMonitor(monitor!)
+        monitor = nil
+      }
+    }
 }
